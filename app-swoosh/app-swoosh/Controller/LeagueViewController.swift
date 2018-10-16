f74f3bff5b8dc9ab2f89d331c79d12c16e823245
Patrick Curran <patrick.e.curran@gmail.com>
2018-10-15 20:53:41 -0400

    initial LeagueViewController implementation

--- app-swoosh/app-swoosh/Controller/LeagueViewController.swift
+++ app-swoosh/app-swoosh/Controller/LeagueViewController.swift
@@ -10,14 +10,34 @@ import UIKit
 
 class LeagueViewController: UIViewController {
 
+    var player: Player!
+    
+    @IBOutlet weak var nextButton: BorderButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        player = Player()
+    }
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextButton.isEnabled = true
     }
     
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillViewControllerSegue", sender: self)
     }
-    
 }
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Patrick Curran on 10/15/18.
+//  Copyright © 2018 Patrick Curran. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
